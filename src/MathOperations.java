2f425b181f4adab09214919c85ee70fc9035fa25
Monika Żmudziak <monika.zmudziak@gmail.com>
2021-12-30 14:18:04 +0100

    Math Operations, Conditional and Control flow, Modulo

--- src/MathOperations.java
+++ src/MathOperations.java
@@ -12,7 +12,7 @@ public class MathOperations {
 
         System.out.println(a / b); // = 2
         System.out.println(10.0 / 4.0); // = 2.5
-        System.out.println(a/b); // = 2.5
+        System.out.println(a / b); // = 2.5
 
         //Comparison operators
         int numOne = 1;
@@ -32,5 +32,49 @@ public class MathOperations {
         System.out.println(numOne < numTwo && numOne == 1); // true && true -> true
         System.out.println(numOne > numTwo && numOne == 1); // false && true -> false
         System.out.println(numOne > numTwo && numOne != 1); // false && false -> false
+
+        // Conditional and Control Flow
+        // if else - we are checking if something is true of false
+        if (numOne > numTwo) {
+            System.out.println("number one is less than number two");
+        } else {
+            System.out.println("number one is greater than number two");
+        }
+
+        // switch - takes a string and executes the appropriate part of the code
+
+        String homePage = "Home";
+        String loginPage = "Login";
+        String contactPage = "Contact";
+
+        String page = homePage; // "jakaś strona"
+
+        switch (page) {
+            case "Home":
+                System.out.println("Przełączam się do strony Home");
+                break;
+            case "Login":
+                System.out.println("Przełączam się do strony Login");
+                break;
+            case "Contact":
+                System.out.println("Przełączam się do strony Contact");
+                break;
+            default:
+                System.out.println("Nie znam takiej strony");
+
+        }
+
+        //Modulo
+
+        System.out.println(10 % 5);
+        System.out.println(10 % 3); // 10 % 3 = 3 r.1<--
+
+
+        if (10 % 2 == 0) {
+            System.out.println("jest liczbą parzystą");
+        } else {
+            System.out.println("nie jest liczbą parzystą");
+        }
     }
 }
+
